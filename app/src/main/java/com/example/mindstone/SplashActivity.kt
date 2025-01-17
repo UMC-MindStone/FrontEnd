8567fd7b4841a5bbf107cecc0ab4bb3d36f3ae53
Zuyeonn <162269448+Zuyeonn@users.noreply.github.com>
2025-01-17 17:27:13 +0900

    [#7] Feat: 로그인1,2 기능 구현

--- app/src/main/java/com/example/mindstone/SplashActivity.kt
+++ app/src/main/java/com/example/mindstone/SplashActivity.kt
@@ -1,20 +1,24 @@
 package com.example.mindstone
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.mindstone.auth.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // 스플래시 -> 2초 후 MainActivity로 이동
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish() // SplashActivity 종료
+        }, 2000) // 2초 딜레이
+
     }
 }
--- app/src/main/java/com/example/mindstone/auth/FindPasswordFragment.kt
+++ app/src/main/java/com/example/mindstone/auth/FindPasswordFragment.kt
@@ -6,11 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.mindstone.R
-import com.example.mindstone.databinding.FragmentFindEmailBinding
+import com.example.mindstone.databinding.FragmentFindPasswordBinding
 
-class FindEmailFragment : Fragment() {
 
-    private var _binding: FragmentFindEmailBinding? = null
+class FindPasswordFragment : Fragment() {
+
+    private var _binding: FragmentFindPasswordBinding? = null
     private val binding get() = _binding!!
 
 
@@ -18,25 +19,25 @@ class FindEmailFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_find_email, container, false)
+        //return inflater.inflate(R.layout.fragment_find_password, container, false)
 
-        _binding = FragmentFindEmailBinding.inflate(inflater, container, false)
+        _binding = FragmentFindPasswordBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // "비밀번호 재설정" 클릭 시 -> FindPasswordFragment로 전환
-        binding.findPWTabLl.setOnClickListener {
-            (activity as? LoginActivity2)?.replaceFragment(FindPasswordFragment())
+        // "이메일 찾기" 버튼 클릭 시 -> FindEmailFragment로 전환
+        binding.findEmailTabLl.setOnClickListener {
+            (activity as? LoginActivity2)?.replaceFragment(FindEmailFragment())
         }
 
         // 뒤로 가기 "<" 버튼 클릭 시 -> LoginActivity로 이동
@@ -44,6 +45,7 @@ class FindEmailFragment : Fragment() {
             activity?.finish() // LoginActivity2 종료 → LoginActivity로 자동 이동
         }
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
