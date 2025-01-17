e66dd43ffc3fd14375b1222b3f77671234e4a092
keypang <bestnest820@naver.com>
2025-01-17 19:45:59 +0900

    [#13] FEAT : 화면 전환 구현

--- app/src/main/java/com/example/mindstone/AfterLoginActivity.kt
+++ app/src/main/java/com/example/mindstone/AfterLoginActivity.kt
@@ -1,20 +1,26 @@
 package com.example.mindstone
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mindstone.databinding.ActivityAfterLoginBinding
 
 class AfterLoginActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityAfterLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_after_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityAfterLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.afterLoginNextIv.setOnClickListener {
+            val intent = Intent(this, MakeNicknameActivity::class.java)
+            startActivity(intent)
+
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/mindstone/MakeNicknameActivity.kt
+++ app/src/main/java/com/example/mindstone/MakeNicknameActivity.kt
@@ -1,20 +1,26 @@
 package com.example.mindstone
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mindstone.databinding.ActivityMakeNicknameBinding
 
 class MakeNicknameActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMakeNicknameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_make_nickname)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMakeNicknameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.makeNicknameBackIv.setOnClickListener {
+            val intent = Intent(this, AfterLoginActivity::class.java)
+            startActivity(intent)
+
+            finish()
         }
     }
 }
