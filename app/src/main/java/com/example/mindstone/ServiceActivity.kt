90b8d366865d53ee2c9ad1c3d769809a80c8d737
keypang <bestnest820@naver.com>
2025-01-17 15:00:56 +0900

    [#9] FEAT : 약관 동의 체크 버튼 구현

--- app/src/main/java/com/example/mindstone/ServiceActivity.kt
+++ app/src/main/java/com/example/mindstone/ServiceActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mindstone
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,6 +23,50 @@ class ServiceActivity : AppCompatActivity() {
 
         binding.serviceGo2Iv.setOnClickListener {
             openFragment(PrivacyFragment())
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        binding.serviceUncheck1Iv.setOnClickListener {
+            binding.serviceUncheck1Iv.visibility = View.GONE
+            binding.serviceChecked1Iv.visibility = View.VISIBLE
+        }
+
+        binding.serviceChecked1Iv.setOnClickListener {
+            binding.serviceUncheck1Iv.visibility = View.VISIBLE
+            binding.serviceChecked1Iv.visibility = View.GONE
+        }
+
+        binding.serviceUncheck2Iv.setOnClickListener {
+            binding.serviceUncheck2Iv.visibility = View.GONE
+            binding.serviceChecked2Iv.visibility = View.VISIBLE
+        }
+
+        binding.serviceChecked2Iv.setOnClickListener {
+            binding.serviceUncheck2Iv.visibility = View.VISIBLE
+            binding.serviceChecked2Iv.visibility = View.GONE
+        }
+
+        binding.serviceUncheck3Iv.setOnClickListener {
+            binding.serviceUncheck3Iv.visibility = View.GONE
+            binding.serviceChecked3Iv.visibility = View.VISIBLE
+        }
+
+        binding.serviceChecked3Iv.setOnClickListener {
+            binding.serviceUncheck3Iv.visibility = View.VISIBLE
+            binding.serviceChecked3Iv.visibility = View.GONE
+        }
+
+        binding.serviceUncheck4Iv.setOnClickListener {
+            binding.serviceUncheck4Iv.visibility = View.GONE
+            binding.serviceChecked4Iv.visibility = View.VISIBLE
+        }
+
+        binding.serviceChecked4Iv.setOnClickListener {
+            binding.serviceUncheck4Iv.visibility = View.VISIBLE
+            binding.serviceChecked4Iv.visibility = View.GONE
         }
     }
 
