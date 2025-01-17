c1cf6e0eb53a3cf7b5d13c2517134f0162fc6978
keypang <bestnest820@naver.com>
2025-01-17 15:17:24 +0900

    [#9] FEAT : 약관 동의 체크 버튼 상태에 맞춰 전체 동의 버튼 상태 변경 구현

--- app/src/main/java/com/example/mindstone/ServiceActivity.kt
+++ app/src/main/java/com/example/mindstone/ServiceActivity.kt
@@ -29,44 +29,127 @@ class ServiceActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
+        //전체 동의 버튼 ON
         binding.serviceUncheck1Iv.setOnClickListener {
             binding.serviceUncheck1Iv.visibility = View.GONE
             binding.serviceChecked1Iv.visibility = View.VISIBLE
+
+            if(binding.serviceUncheck2Iv.visibility == View.VISIBLE){
+                binding.serviceUncheck2Iv.visibility = View.GONE
+                binding.serviceChecked2Iv.visibility = View.VISIBLE
+            }
+
+            if(binding.serviceUncheck3Iv.visibility == View.VISIBLE){
+                binding.serviceUncheck3Iv.visibility = View.GONE
+                binding.serviceChecked3Iv.visibility = View.VISIBLE
+            }
+
+            if(binding.serviceUncheck4Iv.visibility == View.VISIBLE){
+                binding.serviceUncheck4Iv.visibility = View.GONE
+                binding.serviceChecked4Iv.visibility = View.VISIBLE
+            }
         }
 
+        //전체 동의 버튼 OFF
         binding.serviceChecked1Iv.setOnClickListener {
             binding.serviceUncheck1Iv.visibility = View.VISIBLE
             binding.serviceChecked1Iv.visibility = View.GONE
+
+            binding.serviceUncheck2Iv.visibility = View.VISIBLE
+            binding.serviceChecked2Iv.visibility = View.GONE
+
+            binding.serviceUncheck3Iv.visibility = View.VISIBLE
+            binding.serviceChecked3Iv.visibility = View.GONE
+
+            binding.serviceUncheck4Iv.visibility = View.VISIBLE
+            binding.serviceChecked4Iv.visibility = View.GONE
         }
 
+
+        //(필수) 만 14세 이상입니다. ON
         binding.serviceUncheck2Iv.setOnClickListener {
             binding.serviceUncheck2Iv.visibility = View.GONE
             binding.serviceChecked2Iv.visibility = View.VISIBLE
+
+            //버튼이 모두 ON 상태가 되면 전체 동의 버튼 ON
+            if(binding.serviceUncheck2Iv.visibility == View.GONE &&
+                binding.serviceUncheck3Iv.visibility == View.GONE &&
+                binding.serviceUncheck4Iv.visibility == View.GONE){
+
+                binding.serviceUncheck1Iv.visibility = View.GONE
+                binding.serviceChecked1Iv.visibility = View.VISIBLE
+
+            }
         }
 
+        //(필수) 만 14세 이상입니다. OFF
         binding.serviceChecked2Iv.setOnClickListener {
             binding.serviceUncheck2Iv.visibility = View.VISIBLE
             binding.serviceChecked2Iv.visibility = View.GONE
+
+            //버튼이 하나라도 OFF가 되면 전체 동의 버튼 OFF
+            if(binding.serviceChecked1Iv.visibility == View.VISIBLE){
+                binding.serviceUncheck1Iv.visibility = View.VISIBLE
+                binding.serviceChecked1Iv.visibility = View.GONE
+            }
         }
 
+
+        //(필수) 서비스 이용 약관 ON
         binding.serviceUncheck3Iv.setOnClickListener {
             binding.serviceUncheck3Iv.visibility = View.GONE
             binding.serviceChecked3Iv.visibility = View.VISIBLE
+
+            //버튼이 모두 ON 상태가 되면 전체 동의 버튼 ON
+            if(binding.serviceUncheck2Iv.visibility == View.GONE &&
+                binding.serviceUncheck3Iv.visibility == View.GONE &&
+                binding.serviceUncheck4Iv.visibility == View.GONE){
+
+                binding.serviceUncheck1Iv.visibility = View.GONE
+                binding.serviceChecked1Iv.visibility = View.VISIBLE
+
+            }
         }
 
+        //(필수) 서비스 이용 약관 OFF
         binding.serviceChecked3Iv.setOnClickListener {
             binding.serviceUncheck3Iv.visibility = View.VISIBLE
             binding.serviceChecked3Iv.visibility = View.GONE
+
+            //버튼이 하나라도 OFF가 되면 전체 동의 버튼 OFF
+            if(binding.serviceChecked1Iv.visibility == View.VISIBLE){
+                binding.serviceUncheck1Iv.visibility = View.VISIBLE
+                binding.serviceChecked1Iv.visibility = View.GONE
+            }
         }
 
+
+        //(필수) 개인정보 수집 및 이용 동의 ON
         binding.serviceUncheck4Iv.setOnClickListener {
             binding.serviceUncheck4Iv.visibility = View.GONE
             binding.serviceChecked4Iv.visibility = View.VISIBLE
+
+            //버튼이 모두 ON 상태가 되면 전체 동의 버튼 ON
+            if(binding.serviceUncheck2Iv.visibility == View.GONE &&
+                binding.serviceUncheck3Iv.visibility == View.GONE &&
+                binding.serviceUncheck4Iv.visibility == View.GONE){
+
+                binding.serviceUncheck1Iv.visibility = View.GONE
+                binding.serviceChecked1Iv.visibility = View.VISIBLE
+
+            }
         }
 
+        //(필수) 개인정보 수집 및 이용 동의 OFF
         binding.serviceChecked4Iv.setOnClickListener {
             binding.serviceUncheck4Iv.visibility = View.VISIBLE
             binding.serviceChecked4Iv.visibility = View.GONE
+
+            //버튼이 하나라도 OFF가 되면 전체 동의 버튼 OFF
+            if(binding.serviceChecked1Iv.visibility == View.VISIBLE){
+                binding.serviceUncheck1Iv.visibility = View.VISIBLE
+                binding.serviceChecked1Iv.visibility = View.GONE
+            }
         }
     }
 
