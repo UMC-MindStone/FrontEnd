5b204ed5515a150f1faa4109c51f9b6cf57934f7
eeeeeaaan <93918727+eeeeeaaan@users.noreply.github.com>
2025-01-20 15:56:25 +0900

    [#15] UI : 자동 일기 제작(이미지X)

---
+++ app/src/main/java/com/example/mindstone/DiaryCompleteFragment.kt
@@ -0,0 +1,30 @@
+package com.example.mindstone
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mindstone.databinding.FragmentDiaryCompleteBinding
+
+
+class DiaryCompleteFragment : Fragment() {
+    private var _binding : FragmentDiaryCompleteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding= FragmentDiaryCompleteBinding.inflate(inflater,container,false)
+        return binding.root
+
+    }
+
+
+}
