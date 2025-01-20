0120b4ea5a4ea8abdf1e2626aa30eab17e9c13cc
eeeeeaaan <93918727+eeeeeaaan@users.noreply.github.com>
2025-01-20 16:47:14 +0900

    [#15] UI : 일기 수정 화면

---
+++ app/src/main/java/com/example/mindstone/DiaryEditFragment.kt
@@ -0,0 +1,36 @@
+package com.example.mindstone
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mindstone.databinding.FragmentDiaryEditBinding
+
+
+class DiaryEditFragment : Fragment() {
+    private var _binding : FragmentDiaryEditBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDiaryEditBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initClicker(){
+
+    }
+
+}
