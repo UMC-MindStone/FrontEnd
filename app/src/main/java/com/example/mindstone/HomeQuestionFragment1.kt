e11b36e1c01750bb1c845a399ed686564677928b
eeeeeaaan <93918727+eeeeeaaan@users.noreply.github.com>
2025-01-22 16:14:11 +0900

    [#15] UI : 오늘의 질문 최종 화면

--- app/src/main/java/com/example/mindstone/HomeQuestionFragment1.kt
+++ app/src/main/java/com/example/mindstone/HomeQuestionFragment1.kt
@@ -32,4 +32,13 @@ class HomeQuestionFragment1 : Fragment() {
     private fun initClicker(){
 
     }
+
+    private fun statusChange(){
+        // 상태바 변경 로직 작성
+    }
+
+    private fun editing(){
+        // 현재 텍스트가 작성되면 아래에 나타나는 글자 "확인"의 visibility를 gone으로 해두었습니다.
+        // id: overlayEditText에 내용이 단 한글자라도 들어오면 확인 텍스트가 visible로 바뀌고 눌렀을때 다음 화면으로 넘어가도록 합니다.
+    }
 }
