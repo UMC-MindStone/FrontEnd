e2c81c61460eb0b64e529a4494e5e73311325346
eeeeeaaan <93918727+eeeeeaaan@users.noreply.github.com>
2025-01-22 16:09:20 +0900

    [#15] UI : 오늘의 일기 자동완성

---
+++ app/src/main/java/com/example/mindstone/AutoCreatedTodayFragment.kt
@@ -0,0 +1,31 @@
+package com.example.mindstone
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mindstone.databinding.FragmentAutoCreatedTodayBinding
+
+
+class AutoCreatedTodayFragment : Fragment() {
+
+    private var _binding : FragmentAutoCreatedTodayBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAutoCreatedTodayBinding.inflate(inflater,container,false)
+        return binding.root
+
+    }
+
+
+}
