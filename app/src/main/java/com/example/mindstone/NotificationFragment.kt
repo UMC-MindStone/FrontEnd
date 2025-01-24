0d68a0936ea7361dffdf2134a623980980a5e665
eeeeeaaan <93918727+eeeeeaaan@users.noreply.github.com>
2025-01-24 21:09:34 +0900

    [#15] UI : 알림 화면 Adapter 연결

--- app/src/main/java/com/example/mindstone/NotificationFragment.kt
+++ app/src/main/java/com/example/mindstone/NotificationFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mindstone.databinding.FragmentNotificationBinding
 
 
@@ -24,6 +25,33 @@ class NotificationFragment : Fragment() {
     ): View {
         _binding = FragmentNotificationBinding.inflate(layoutInflater)
         return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // mock data
+        val notificationList = arrayListOf(
+            NotificationItem(
+                notiIcon = R.drawable.ic_emotion,
+                notiText = "습관 기록 안한지 1일이 지났어요!",
+                notiDate = "10분 전"
+            ),
+            NotificationItem(
+                notiIcon = R.drawable.ic_emotion,
+                notiText = "오늘의 일기를 기록해볼까요?",
+                notiDate = "1일 전"
+            )
+        )
+
+        // RecyclerView와 Adapter 설정
+        val adapter = NotiRVAdapter(notificationList)
+        binding.notiRl.apply{
+            this.adapter = adapter
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+        }
 
     }
 
