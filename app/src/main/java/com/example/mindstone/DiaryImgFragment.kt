0a7ccd8abebd885f21a90ad150ca90af40cc9e76
eeeeeaaan <93918727+eeeeeaaan@users.noreply.github.com>
2025-01-25 09:49:41 +0900

    [#15] Feat : 이미지 삽입 구현

--- app/src/main/java/com/example/mindstone/DiaryImgFragment.kt
+++ app/src/main/java/com/example/mindstone/DiaryImgFragment.kt
@@ -1,16 +1,34 @@
 package com.example.mindstone
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.mindstone.databinding.FragmentDiaryImgBinding
+import java.net.URI
 
 
 class DiaryImgFragment : Fragment() {
     private var _binding: FragmentDiaryImgBinding? = null
     private val binding get() = _binding!!
+
+    private val imageViews = mutableListOf<ImageView>()
+    private var imageCnt = 0
+
+    private val selectImageLauncher = registerForActivityResult(ActivityResultContracts.GetContent()){ uri: Uri? ->
+        uri?.let{
+            if(imageCnt < 4){
+                addImage(it)
+            } else {
+                binding.diaryAddImgTv.visibility = View.INVISIBLE
+            }
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +42,36 @@ class DiaryImgFragment : Fragment() {
         _binding= FragmentDiaryImgBinding.inflate(inflater,container,false)
         return binding.root
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        imageViews.addAll(
+            listOf(
+                binding.diaryImg1Iv,
+                binding.diaryImg2Iv,
+                binding.diaryImg3Iv,
+                binding.diaryImg4Iv
+            )
+        )
+
+        binding.diaryAddImgIv.setOnClickListener{
+            selectImageLauncher.launch("image/*")
+        }
+    }
+
+    private fun addImage(uri : Uri){
+        if (imageCnt < 4){
+            val imageView = imageViews[imageCnt]
+            imageView.setImageURI(uri)
+            imageViews.add(imageView)
+            imageCnt++
+        }
+    }
+    override fun onDestroy(){
+        super.onDestroy()
+        _binding = null
     }
 
 
