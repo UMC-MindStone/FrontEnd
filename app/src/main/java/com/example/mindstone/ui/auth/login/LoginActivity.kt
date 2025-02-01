d5658fbde818a1762a6bdd6f4618a647827b175e
eeeeeaaan <93918727+eeeeeaaan@users.noreply.github.com>
2025-02-01 19:22:15 +0900

    [#39] auth/login .kt 뷰 조정

--- app/src/main/java/com/example/mindstone/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/example/mindstone/ui/auth/login/LoginActivity.kt
@@ -30,13 +30,12 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
 
         // ViewBinding 초기화
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
