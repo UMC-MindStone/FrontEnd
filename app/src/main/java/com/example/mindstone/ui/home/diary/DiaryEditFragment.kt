41cd3773a53fcd7e729d7253092e5f87d3bfe8ee
eeeeeaaan <93918727+eeeeeaaan@users.noreply.github.com>
2025-02-18 21:18:28 +0900

    [#52] FEAT: 함수 파라미터 변경사항 반영

--- app/src/main/java/com/example/mindstone/ui/home/diary/DiaryEditFragment.kt
+++ app/src/main/java/com/example/mindstone/ui/home/diary/DiaryEditFragment.kt
@@ -1,6 +1,7 @@
 package com.example.mindstone.ui.home.diary
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -86,12 +87,14 @@ class DiaryEditFragment : Fragment() {
 
     private fun initClicker(){
         binding.diaryEditCompleteIv.setOnClickListener{
+            Log.d("Upload Debug","✅ 저장 버튼 클릭됨")
             val updatedText = binding.diaryEditTextTv.text.toString()
             diaryViewModel.updateDiaryText(updatedText)
 
             val date = LocalDate.of( currentYear, currentMonth, currentDay)
             val formattedDate = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
-            diaryViewModel.saveOrUpdateDiary(formattedDate, updatedText)
+
+            diaryViewModel.saveOrUpdateDiary(requireContext(),formattedDate, updatedText)
             requireActivity().supportFragmentManager.popBackStack()
         }
 
